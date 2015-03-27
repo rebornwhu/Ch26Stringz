07a1b9524cadbd03c452f6ef1118083713a1885d
Xiao Lu <xiao.lu@c02gm58mdv7n-l.local>
2015-03-26 18:43:02 -0700

    setup

---
+++ Ch26Stringz/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Ch26Stringz
+//
+//  Created by Xiao Lu on 3/26/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
