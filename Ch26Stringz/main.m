986e0641b09368404699b8ec257f80d0a3bb0b30
Xiao Lu <xiao.lu@c02gm58mdv7n-l.local>
2015-03-26 19:32:44 -0700

    Read and write

--- Ch26Stringz/main.m
+++ Ch26Stringz/main.m
@@ -13,9 +13,36 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        /* WRITING FILE */
+        NSMutableString *strWrite =[[NSMutableString alloc] init];
+        for (int i = 0; i < 10; i++) {
+            [strWrite appendString:@"Aaron is cool!\n"];
+        }
         
+        NSError *error;
+        
+        BOOL success = [strWrite writeToFile:@"/tmp/cool.txt"
+              atomically:YES
+                encoding:NSUTF8StringEncoding
+                   error:&error];
+        
+        if (success) {
+            NSLog(@"done writing /tmp/cool.txt");
+        }
+        else {
+            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+        }
+        
+        /* READING FILE */
+        NSString *strRead = [[NSString alloc] initWithContentsOfFile:@"/etc/resolv.conf"
+                                              encoding:NSASCIIStringEncoding
+                                                 error:&error];
+        if (!strWrite) {
+            NSLog(@"read failed: %@", [error localizedDescription]);
+        }
+        else {
+            NSLog(@"resolve.conf looks like this: %@", strRead);
+        }
     }
     return 0;
 }
